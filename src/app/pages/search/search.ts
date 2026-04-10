e30e0dcd39c30da93c446d1abe0d8e363f80091f
Erwan Defossez <erwan.defossez@isen.yncrea.fr>
2026-04-10 14:36:40 +0200

    Page Search : formulaire de recherche avec validation et navigation

--- src/app/pages/search/search.ts
+++ src/app/pages/search/search.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
-export class Search {}
+export class Search {
+
+  private router = inject(Router);
+
+  searchForm = new FormGroup({
+    gameName: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(3)]
+    }),
+    tagLine: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(2)]
+    })
+  });
+
+  onSearch() {
+    if (this.searchForm.valid) {
+      const gameName = this.searchForm.controls.gameName.value;
+      const tagLine = this.searchForm.controls.tagLine.value;
+      this.router.navigate(['/summoner', 'euw', gameName, tagLine]);
+    }
+  }
+}
--- src/app/services/riot-api.ts
+++ src/app/services/riot-api.ts
@@ -10,7 +10,7 @@ export class RiotApiService {
   private http = inject(HttpClient);
 
   // Clé API Riot (à renouveler toutes les 24h)
-  private apiKey = 'Ma clé';
+  private apiKey = 'RGAPI-b467856a-1da4-4418-9822-e88883cfbe90';
 
   private get headers(): HttpHeaders {
     return new HttpHeaders({ 'X-Riot-Token': this.apiKey });
